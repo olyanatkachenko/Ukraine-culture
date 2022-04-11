4754185fa497c346f58901177246ad06bac1d6dd
Ellen Treidler <ellentreidler@gmail.com>
2022-04-11 14:51:04 +0200

    blog 1.0

---
+++ blog.js
@@ -0,0 +1,63 @@
+const {tags, readingTime} = GhostHelpers;
+
+
+
+const ghostapi = new GhostContentAPI({
+    url: 'http://localhost:2368',
+    key: '00cef0bf2fa8d415ce1b5a7b9a',
+    version: "v3"
+});
+
+// fetch 5 posts, including related tags and authors
+ghostapi.posts
+    .browse({limit: 3, include: 'tags,authors'})
+    .then((posts) => {
+        display_posts(posts)
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+
+function display_posts(posts) {
+    let container = document.getElementById("blog-container")
+
+    let postsHtml = ""
+
+
+    posts.forEach((post) => {
+        console.log(post);
+        let title = post.title
+        let date = format_date( post.published_at)
+        let image = post.feature_image
+        let url = post.url
+
+        let time = readingTime(post, {minute: '1 min read.', minutes: '% min read.'});
+        console.log(time)
+
+        let postHTML = `<div class="blog-box">
+
+                <div class="blog-img">
+                    <img src="${image}" alt=""/>
+                </div>
+
+                <div class="blog-text">
+                    <span>${date} • ${time}</span>
+                    <a href="${url}" class="blog-title">${title}</a>
+                    <a href="${url}">Read more</a>
+                </div>
+
+            </div>`
+
+        postsHtml = postsHtml+postHTML
+
+    });
+
+
+    container.innerHTML = postsHtml
+}
+
+function format_date(dateString) {
+    let now = new Date(dateString)
+    return now.toLocaleDateString()
+}
